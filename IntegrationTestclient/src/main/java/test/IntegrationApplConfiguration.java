2850a6d7bf0080889b663bf99f6318097f39848e
Lars Gohr <larsgohr@gmail.com>
2016-10-05 20:56:13 +0200

    Adding example dashboard client application

--- IntegrationTestclient/src/main/java/test/IntegrationApplConfiguration.java
+++ IntegrationTestclient/src/main/java/test/IntegrationApplConfiguration.java
@@ -1,11 +1,26 @@
 package test;
 
 import de.datainspector.DataInspector;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @Profile({"integration", "noApplicationName"})
 @DataInspector
 public class IntegrationApplConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
 }
